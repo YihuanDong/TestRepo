1297480ac213ef24143c09cef465a34995cb78df
Yihuan Dong <ydong2@ncsu.edu>
2016-03-28 23:18:14 -0400

    Changed filepath

--- shell.py
+++ shell.py
@@ -1,12 +1,29 @@
 import subprocess
+import os, traceback
 
 try:
+    print os.listdir("./decision_tree")
+    print "1"
     toolName = "decision_tree"
-    shellPath = "./decision_tree/main.py"
+    shellPath =os.path.join(".","decision_tree","main.py")
+    #shellPath = os.path.abspath(shellPath)
+    #shellPath = ".\\decision_tree\\main.py"
+    print shellPath
+
+    # shellPath = shellPath.replace('\\', '\\\\')
+    #shellPath = '"' + shellPath + '"'
+    print shellPath
+    print "2"
     
-    subprocess.call(["python " + shellPath + " " + toolName],shell=True)
+    filepath = "python " + shellPath + " " + toolName
+    print filepath
+    
+    subprocess.call(["python",shellPath,toolName],shell=True)
+    #subprocess.call(['python E:\\Courses\\CSC_510\\TestRepo\\decision_tree email'],shell = True)
+    print "3"
 except Exception as e:
     print("An error occurred in the system: ")
     print(e)
+    traceback.print_exc()
     print("Please use 'python shell.py " + toolName + "' to run the shell again.")
     print
