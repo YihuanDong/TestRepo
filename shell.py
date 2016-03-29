9cf84fac0e6798dfa6dd3fd83e5873a0174606ac
Yihuan Dong <ydong2@ncsu.edu>
2016-03-28 23:32:28 -0400

    Cleaned shell.py

--- shell.py
+++ shell.py
@@ -3,27 +3,16 @@ import os, traceback
 import sys
 
 try:
-    print os.listdir("./decision_tree")
-    print "1"
     toolName = "decision_tree"
-    shellPath =os.path.join(".","decision_tree","main.py")
-    #shellPath = os.path.abspath(shellPath)
-    #shellPath = ".\\decision_tree\\main.py"
-    print shellPath
-
-    # shellPath = shellPath.replace('\\', '\\\\')
-    #shellPath = '"' + shellPath + '"'
-    print shellPath
-    print "2"
     
+    shellPath =os.path.join(".","decision_tree","main.py")
     filepath = "python " + shellPath + " " + toolName
-    print filepath
+    
     if sys.platform.startswith("win32"):
         subprocess.call(["python",shellPath,toolName],shell=True)
     else:
         subprocess.call([filepath],shell=True)
-    #subprocess.call(['python E:\\Courses\\CSC_510\\TestRepo\\decision_tree email'],shell = True)
-    print "3"
+
 except Exception as e:
     print("An error occurred in the system: ")
     print(e)
